37df884244a8c22971ce84d82972df9c2bcad6a5
arjunfer <arjunferinggi97@gmail.com>
2022-07-07 16:17:52 +0700

    1

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { refreshToken } from "../controllers/refreshToken.js";
+const router = express.Router();
+
+router.get('/users', verifyToken, getUsers);
+router.post('/users', Register);
+router.post('/login', Login);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+router.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+
+export default router;
